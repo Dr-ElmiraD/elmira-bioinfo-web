64eae4ea4889c8bf07d81192f0e40c9cb266c77b
Dr. Elmira Davasaz <elmira.dt@gmail.com>
2025-09-21 04:36:17 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // App Router i18n
   i18n: {
     locales: ["en", "tr", "fr", "fa"],
     defaultLocale: "en",
     localeDetection: true,
   },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
+
